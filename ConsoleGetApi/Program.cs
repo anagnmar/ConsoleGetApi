b79d8c489aec0b1069b520440db1235b2d7d5b55
Μαρίνος Αναγνώστου <managnostou@remaco.gr>
2019-06-28 10:48:19 +0300

    Add project files.

---
+++ ConsoleGetApi/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+
+namespace ConsoleGetApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            GetRequest("https://reqres.in/api/users");
+
+            Console.ReadKey();
+
+        }
+
+        async static void GetRequest(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(url))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        string mycontent = await content.ReadAsStringAsync();
+
+
+                        Console.WriteLine(mycontent);
+                    }
+                }
+            }
+        }
+    }
+}
